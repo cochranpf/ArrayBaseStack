387a6d6431bd461a6fb45e7821ed675098b2fb26
Paul F Cochran <cochranpf@rams.adp.vcu.edu>
2018-02-13 14:29:39 -0500

    Finalized LinkedChainStack

--- LinkedChainStack.java
+++ LinkedChainStack.java
@@ -5,27 +5,44 @@ public class LinkedChainStack<E> implements StackInterface<E> {
    private Node<E> topNode;
    
    public void push(E newElement) {
-      
+      Node no = new Node(newElement);
+      topNode.setNext(no);
+      topNode = no;
    }
    
    public E pop() {
-   
+      if (isEmpty()) {
+         throw new EmptyStackException();
+      }
+      
+      E result = topNode.getData();
+      topNode = topNode.getNext();
+      return result;
    }
    
    public E peek() {
-   
+      if (isEmpty()) {
+         throw new EmptyStackException();
+      }
+      
+      return topNode.getData(); 
    }
   
    public boolean isEmpty() {
-      return true;
+      if (topNode == null) {
+         return true;
+      }
+      return false;
    }
   
    public void clear() {
-   
+      while(!(isEmpty())) {
+         pop();
+      }
    }
 
    
-   private class Node 
+   private class Node<E> 
 	{
 	  private E data;
 	  private Node next;
@@ -40,5 +57,17 @@ public class LinkedChainStack<E> implements StackInterface<E> {
 			data = dataPortion;
 			next = nextNode;	
 		} 
+      
+      public void setNext(Node n) {
+         next = n;
+      }
+      
+      public E getData () {
+         return data;
+      }
+      
+      public Node getNext () {
+         return next;
+      }
 	} 
 }
